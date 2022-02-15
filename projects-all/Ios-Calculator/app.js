74223369d42db72b35a1d92b3482fd23ffebb543
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-02-15 21:55:43 +0300

    update

---
+++ projects-all/Ios-Calculator/app.js
@@ -0,0 +1,32 @@
+// let one = document.getElementById("btn_1");
+// let two = document.getElementById("btn_2");
+// let three = document.getElementById("btn_3");
+// let four = document.getElementById("btn_4");
+// let five = document.getElementById("btn_5");
+// let six = document.getElementById("btn_6");
+// let seven = document.getElementById("btn_7");
+// let eight = document.getElementById("btn_8");
+// let nine = document.getElementById("btn_9");
+// let zero = document.getElementById("btn_0");
+// let proc_plus = document.getElementById("process_Plus");
+// let proc_sub = document.getElementById("process_Sub");
+// let proc_div = document.getElementById("process_Div");
+// let proc_multi = document.getElementById("process_Mult");
+
+
+
+
+// let value;
+// let result;
+
+
+// one.addEventListener("click",() => {
+//     value=1;
+//     document.getElementById("screen2").innerHTML=value;
+    
+// })
+
+let select= document.querySelectorAll("#container").children;
+console.log(select);
+
+
---
+++ projects-all/To-Do-App/app.js
@@ -0,0 +1,17 @@
+let inputBox=document.querySelector(".content input");
+let addBtn=document.querySelector(".content button");
+
+inputBox.onkeyup=() =>{
+    let userData =inputBox.value;
+    if (userData.trim()!= 0) {
+        addBtn.classList.add("active");
+        
+    }else{
+        addBtn.classList.remove("active");
+
+    }
+
+
+
+}
+

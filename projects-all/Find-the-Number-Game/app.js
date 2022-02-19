4a05ce40e9823fe6058cc1d1fd0e02a115df4043
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-02-19 22:46:38 +0300

    update

--- projects-all/Find-the-Number-Game/app.js
+++ projects-all/Find-the-Number-Game/app.js
@@ -1,6 +1,8 @@
 let rd_number= Math.floor(Math.random()*101);
 console.log(rd_number);
 let i=1;
+let up_number=100;
+let down_number=0;
 
 
 let predNumber = document.getElementById("number_text");
@@ -12,20 +14,23 @@ btn.addEventListener("click", ()=>{
     do {
         
         if(pred_number==rd_number){
-            console.log("sayı esittir"+ i +".denemde buldunuz");
-            prompt("sayı esittir"+ i +".denemde buldunuz");
+            //console.log("sayı esittir"+ i +".denemde buldunuz");
+            //pred_number=prompt("sayı esittir"+ i +".denemde buldunuz");
+            document.getElementById("attempts").innerText=i+ ". denemeniz";
             break;
 
         }
         else if( pred_number > rd_number){
-            console.log("tahmin edilen sayı" + pred_number +"sayısından küçüktür." + i +".denemde buldunuz");
-            prompt("tahmin edilen sayı" + pred_number +"sayısından küçüktür." + i +".denemde buldunuz");
+            //console.log("tahmin edilen sayı" + pred_number +"sayısından küçüktür." + i +".denemde buldunuz");
+            pred_number=prompt("tahmin edilen sayı" + pred_number +"sayısından küçüktür." + i +".denemde buldunuz");
             i++;
 
 
         }
         else if(pred_number<rd_number){
             console.log("tahmin edilen sayı" +pred_number +"sayısından büyüktür." +i +".denemde buldunuz");
+            pred_number=prompt( pred_number,"100");
+
             i++;
            
             

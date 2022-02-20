844c4fad5f9a046e82a00e88714e0d656704abd0
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-02-20 15:42:54 +0300

    update

--- projects-all/Ios-Calculator/app.js
+++ projects-all/Ios-Calculator/app.js
@@ -1,33 +1,50 @@
 
-let list_btn=document.getElementsByClassName("Button");
-let list_process=document.getElementsByClassName("Proces1");
-let list_other=document.getElementsByClassName("Proces2")
+const list_btn=document.getElementsByClassName("Button");
+const list_process=document.getElementsByClassName("Proces1");
+const list_other=document.getElementsByClassName("Proces2");
 
-let value;
-let old_value;
-let result;
 
+// list_btn.innerText();
 
 
 
+const tiklama=(op)=>{
+  
 
-for (let i = 0; i < list_btn.length; i++) {
-    // btn_value[i]= list_btn[i].innerHTML
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 0; i < list_btn.length; i++) {
+//     // btn_value[i]= list_btn[i].innerHTML
     
-    list_btn[i].addEventListener("click",() =>{
+//     list_btn[i].addEventListener("click",() =>{
         
-        document.getElementById("screen2").innerHTML+=list_btn[i].innerHTML;
+//         document.getElementById("screen2").innerHTML+=list_btn[i].innerHTML;
 
-    })
+//     })
     
-}
+// }
 
-for (let i = 0; i < list_process.length; i++) {
-    list_process[i].addEventListener("click",()=>{
-        document.getElementById("screen2").innerHTML=list_process[i].innerHTML;
-    })
+// for (let i = 0; i < list_process.length; i++) {
+//     list_process[i].addEventListener("click",()=>{
+//         document.getElementById("screen2").innerHTML=list_process[i].innerHTML;
+//     })
     
-}
+// }
 
 
 
@@ -48,24 +65,3 @@ for (let i = 0; i < list_process.length; i++) {
 
 
 
-
-
-
-
-
-///rakamlar için
-
-
-
-// function yazdir() {
-//     for (let i = 0; i < list_btn.length; i++) {
-//         btn_value[i]= list_btn[i].innerHTML;
-//         // console.log(value);
-//         if (btn_value[i]!="AC"&& btn_value[i]!="=") {
-//             list_btn[i].addEventListener("click", ()=>{
-//                 document.getElementById("screen2").innerHTML=btn_value[i];
-//                 result=btn_value[i];
-//                 console.log(result);
-//                 // return;
-    
-//             });

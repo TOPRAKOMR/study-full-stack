c18c8a059362acda3f7f026b8ee5614e09c316c2
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-02-22 01:38:43 +0300

    update

--- projects-all/Find-the-Number-Game/app.js
+++ projects-all/Find-the-Number-Game/app.js
@@ -3,47 +3,57 @@ function rnd_number_fnc () {
 }
 let rnd_number=rnd_number_fnc ()
 
-
-
 console.log(rnd_number);
-// let i=1;
-// let up_number=100;
-// let down_number=0;
-
 
 let predNumber = document.getElementById("number_text");
 let btn_check = document.getElementById("btn_check");
 let btn_reset = document.getElementById("btn_reset");
 let list_number=document.getElementById("result_list");
+let gues_count=0;
+
 
 btn_check.addEventListener("click", ()=>{
-    // let newLi =document.createElement("li");
-    // let licontent = document.createTextNode(predNumber.value);
 
     let number=predNumber.value;
-    
-    if(number==rnd_number){
-        list_number.innerHTML+= '<li>' + number +' (too low)</li>';
+    if (number > 0 && number < 101){
+        if(number==rnd_number){
+            gues_count++;
+            list_number.innerHTML+= '<li>'+gues_count+ '. Guess  Congratulations!  You won It.  Guess number: '+ number + '</li>';
+            document.getElementById("attempts").innerHTML=gues_count;
         
+            
+        }
+        else if( number > rnd_number){
+            gues_count++;
+            list_number.innerHTML+= '<li>'+gues_count+ '. Guess: ' + number +'  Wrong! Last guess was too high! </li>';
+            document.getElementById("attempts").innerHTML=gues_count;
     
-        
+    
+            }
+        else if(number<rnd_number){
+            gues_count++;
+            list_number.innerHTML+= '<li>'+gues_count+ '. Guess: ' + number +'  Wrong! Last guess was too low! </li>';
+            document.getElementById("attempts").innerHTML=gues_count;
+            }
+ 
     }
-    else if( number > rnd_number){
-        list_number.innerHTML+= '<li>' + number +' (too low)</li>';
-
-        i++;
-
-
-        }
-    else if(number<rnd_number){
-        list_number.innerHTML+= '<li>' + number +' (too low)</li>';
-
-        i++;
-        
-        }
-        
+    else alert(" Please enter a number between 1-100");
 
 })
+
+btn_reset.addEventListener("click",()=>{
+    const rnd_number=rnd_number_fnc ();
+    list_number.innerHTML="";
+    document.getElementById("attempts").innerHTML="";
+    gues_count=0;
+
+
+
+
+
+})
+
+
 
 
 

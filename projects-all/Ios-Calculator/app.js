d4e187da0b43eccb1df5a8687b79fb5ea49f4792
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-02-21 14:04:57 +0300

    update

--- projects-all/Ios-Calculator/app.js
+++ projects-all/Ios-Calculator/app.js
@@ -1,55 +1,7 @@
 
 const list_btn=document.getElementsByClassName("Button");
-const list_process=document.getElementsByClassName("Proces1");
-const list_other=document.getElementsByClassName("Proces2");
-
-
-// list_btn.innerText();
-
-
-
-const tiklama=(op)=>{
-  
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for (let i = 0; i < list_btn.length; i++) {
-//     // btn_value[i]= list_btn[i].innerHTML
-    
-//     list_btn[i].addEventListener("click",() =>{
-        
-//         document.getElementById("screen2").innerHTML+=list_btn[i].innerHTML;
-
-//     })
-    
-// }
-
-// for (let i = 0; i < list_process.length; i++) {
-//     list_process[i].addEventListener("click",()=>{
-//         document.getElementById("screen2").innerHTML=list_process[i].innerHTML;
-//     })
-    
-// }
-
-
-
-
-
+const pre_display=document.getElementsByClassName("Proces1");
+const current_display=document.getElementsByClassName("buttons-container");
 
 
 

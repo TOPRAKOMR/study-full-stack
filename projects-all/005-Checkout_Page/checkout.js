49b3c3729766ac96e84907966669065067f214d6
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-02-24 23:45:01 +0300

    update

--- projects-all/005-Checkout_Page/checkout.js
+++ projects-all/005-Checkout_Page/checkout.js
@@ -13,17 +13,79 @@ window.addEventListener("load",()=>{
 
 let productsDiv =document.querySelector(".products");
 productsDiv.addEventListener("click",(event)=>{
-    console.log(event.target);
+    let quantityP= event.target.parentElement.parentElement.querySelector("#product-quantity");
+    // console.log(quantityP);
+    // console.log(event.target);
     // minus buttons
-    if(event.target.classList.contains("fa-minus" || )){
-        console.log("minus clicked");
+    if(event.target.classList.contains("fa-minus")|| event.target==quantityP.parentElement.firstElementChild ){
+        // console.log("minus clicked");
+        if(quantityP.innerText>1){
+            quantityP.innerText--;
+            //calculate ProductTotal
+            //Cart total
+            calculateCartTotal(quantityP);
+
+        }
+        else{
+            if (confirm("sillinecek kardes")){
+                quantityP.parentElement.parentElement.parentElement.remove();
+                //Cart total
+                calculateCartTotal();
+
+
+            };
+        }
+        
+
     }
-    else if(event.target.className=="fas fa-plus"){
-        console.log("plusBtn clicked")
+    else if(event.target.className=="fas fa-plus" || event.target==quantityP.parentElement.lastElementChild ){
+        // console.log("plusBtn clicked")
+        quantityP.innerText++;
+        //calculate ProductTotal
+        //Cart total
+        calculateCartTotal(quantityP);
+
     }
+
+
+    // remove buttons..
     else if(event.target.className=="remove-product"){
-        console.log("Remove Btn clicked")
+        // console.log("Remove Btn clicked")
+        quantityP.parentElement.parentElement.parentElement.remove();
+        //Cart total
+
     }
 
 })
+
+
+
+
+const calculateCartTotal=()=>{
+
+let productTotalPriceDivs=document.querySelectorAll("product-line-price");
+let subtotal=0;
+productTotalPriceDivs.forEach(eacproducttotalPriceDiv =>{
+    subtotal+=parseFloat(eacproducttotalPriceDiv.innerText)
+});
+console.log(subtotal);
+let taxPrice=subtotal*localStorage.getItem("taxRate");
+console.log(taxPrice);
+let shipping=(subtotal>0 ? parseFloat(localStorage.getItem(shippingPrice)):0);
+console.log(shipping);
+let cartTotal=subtotal+taxPrice+shipping;
+console.log(cartTotal);
+
+document.querySelector("#cart-subtotal p:nth-child(2)").innerText=subtotal.toFixed(2);
+document.querySelector("#cart-tax p:nth-child(2)").innerText=taxPrice.toFixed(2);
+document.querySelector("#cart-sipping p:nth-child(2)").innerText=shipping.toFixed(2);
+document.querySelector("#cart-total p:nth-child(2)").lastElementChild.innerText=cartTotal.toFixed(2);
+
+
+
+}
+
+const calculateProductTotal=(quantityP)=>{
+    let productPrice=quantityP.parentElement.parentElement
+}
 

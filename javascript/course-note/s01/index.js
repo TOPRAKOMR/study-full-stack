cf3bdd0e64f4aa8b711f3771ae87d4cf8470d979
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-03-16 21:13:20 +0300

    update

--- javascript/course-note/s01/index.js
+++ javascript/course-note/s01/index.js
@@ -1,6 +1,28 @@
-console.log('Hello World!');
-console.log('***********');
-document.write('clarusway!!')
-console.log(5+3)
+// console.log('Hello World!');
+// console.log('***********');
+// document.write('clarusway!!')
+// console.log(5+3)
+
+
+let str="(2x^2+5)(3x^2-5)";
+
+str=str.split(")(");
+lng=str.length;
+str[0]=str[0].replace("(","");
+// console.log(lng);
+str[lng-1]=str[lng-1].replace(")","");
+let y=[];
+
+str.forEach((x) => { y=x.split("+")
+// console.log(y);
+});
+
+
+
+
+console.log(y);
+
+// console.log(y);
+
 
 

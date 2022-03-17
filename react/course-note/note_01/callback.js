87de257c467df845050486b67532243683416400
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-03-17 20:53:00 +0300

    update

--- react/course-note/note_01/callback.js
+++ react/course-note/note_01/callback.js
@@ -15,6 +15,34 @@
 //     console.log("Hello");
 // });
 
- import fetch from "node-fetch";
- fetch("https://jsonplaceholder.typicode.com/users").then((data)=>data.json())
- .then((users)=>{console.log(users)});
+ import axios from "axios";
+import fetch from "node-fetch";
+//  fetch("https://jsonplaceholder.typicode.com/users").then((data)=>data.json())
+//  .then((users)=>{
+//      fetch("https://jsonplaceholder.typicode.com/posts/1").then((data)=>data.json())
+//      .then((post)=>{console.log("Post Yklendi", post)})
+
+//     });
+
+
+// async function getData() {
+//     const post=await(
+//         fetch("https://jsonplaceholder.typicode.com/posts/1")).json();
+//     const users=await(
+//         fetch("https://jsonplaceholder.typicode.com/users")
+//     ).json();
+//     console.log("Users:",users);
+//     console.log("Post 1",post);
+
+
+// }
+// getData();
+
+(async()=>{
+    // const users=await axios("https://jsonplaceholder.typicode.com/users");
+    const {data}=await axios("https://jsonplaceholder.typicode.com/users");
+    const {data: post1}= await axios("https://jsonplaceholder.typicode.com/posts/1");
+    // console.log("Users:",users);
+    console.log("Users:",data);
+    console.log("Post1;",post1);
+})();

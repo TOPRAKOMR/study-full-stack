ef39cb9c5d83a6e843a16a684ebaf90687e3365a
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-03-22 12:25:09 +0300

    update

--- react/course-note/react-s01/src/index.js
+++ react/course-note/react-s01/src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const Child=()=><p>I am child </p>;
+
+const App=()=>{
+return(
+  <>
+
+  <Child/>
+  <Child/>
+  <Child/>
+  <Child/>
+  <p>asdbfaskdfkasbd</p>
+
+  </>
 );
+
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

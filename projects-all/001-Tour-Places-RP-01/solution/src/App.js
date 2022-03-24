69af117e6a8f61721c04ec8270a0d44b670cdc5b
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-03-24 11:52:54 +0300

    update

--- projects-all/001-Tour-Places-RP-01/solution/src/App.js
+++ projects-all/001-Tour-Places-RP-01/solution/src/App.js
@@ -1,17 +1,14 @@
-import "./App.scss";
-import Header from "./components/Header/Header";
-import Navbar from "./components/Navbar/Navbar";
-import Main from "./components/Main/Main";
+import"./App.scss";
 
 
-const App = () => {
+const App =()=>{
   return(
     <div className="App">
-      <Navbar/>
-      <Header/>
-      <Main/>
+      <h1> Welcome React</h1>
+      <p>lorem ipsum</p>
     </div>
+
   )
 }
 
-export default App;
+

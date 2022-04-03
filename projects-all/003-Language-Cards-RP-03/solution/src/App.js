88b0049076c32b26deb04f724669f0b265ec8efd
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-04-03 15:01:34 +0300

    update

--- projects-all/003-Language-Cards-RP-03/solution/src/App.js
+++ projects-all/003-Language-Cards-RP-03/solution/src/App.js
@@ -6,7 +6,6 @@ import Header from './components/Header/Header';
 function App() {
   return (
     <div className="main-container">
-      Language card project
       <Header/>
       <Card/>
       

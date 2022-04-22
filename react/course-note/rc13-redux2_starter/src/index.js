0df404db34cd2c10863d74b8e7f37ea75da57e34
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-04-22 12:04:24 +0300

    update

---
+++ react/course-note/rc13-redux2_starter/src/index.js
@@ -0,0 +1,14 @@
+import App from "./App";
+import { createRoot } from "react-dom/client";
+import store from "./redux/store";
+import { Provider } from "react";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+<Provider store={store}>
+    <App />
+
+</Provider>
+
+);

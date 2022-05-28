5302b5479da5c0a45c0c8caffdb3e7a44192823d
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-05-28 17:36:36 +0300

    update

--- django/course-note/008_Forms/student/views.py
+++ django/course-note/008_Forms/student/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+
+from django.shortcuts import render,redirect
+from .forms import StudentForm
 
 # Create your views here.
 def index(request):
     return render(request, 'student/index.html')
 
 def student_page(request):
-    return render(request,'student/student.html')
+    # print(request.POST)
+
+
+    form=StudentForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('index')
+    context={
+        'form':form
+
+    }
+    return render(request,'student/student.html',context)

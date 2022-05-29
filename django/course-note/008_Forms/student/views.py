f8c8885036652ae3d33e25d42ab182dd894c85d5
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-05-29 21:44:52 +0300

    update

--- django/course-note/008_Forms/student/views.py
+++ django/course-note/008_Forms/student/views.py
@@ -1,6 +1,7 @@
 
 from django.shortcuts import render,redirect
 from .forms import StudentForm
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -13,7 +14,11 @@ def student_page(request):
     form=StudentForm(request.POST or None)
 
     if form.is_valid():
-        form.save()
+        student=form.save()
+        if 'profile_pic' in request.FILES:
+            student.profile_pic=request.FILES['profile_pic']
+            student.save()
+        messages.success(request,"Student saved successfully")
         return redirect('index')
     context={
         'form':form

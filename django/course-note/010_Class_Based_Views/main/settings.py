243188f08f02dc03e6f9f13a0f56417cd2ec86d7
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-05-30 23:17:41 +0300

    update

--- django/course-note/010_Class_Based_Views/main/settings.py
+++ django/course-note/010_Class_Based_Views/main/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from distutils.command.config import config
 from pathlib import Path
+
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$g@-5wbo6ujmu14t9s1)p4^mimy+u#tg9#h=@@_9t%vb^q6bk-'
+
+
+SECRET_KEY=config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fscohort'
 ]
 
 MIDDLEWARE = [

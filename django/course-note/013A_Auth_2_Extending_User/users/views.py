435d417eea8e2da1507fe4723c6d8eb51541f33f
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-06-01 22:35:55 +0300

    update

---
+++ django/course-note/013A_Auth_2_Extending_User/users/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render,redirect,HttpResponse
+
+from django.contrib import messages
+from django.contrib.auth import logout,login,authenticate
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'users/home.html')
+
+
+def user_logout(request):
+    messages.success(request,'You logout')
+    logout(request)
+    return redirect('home')
+
+

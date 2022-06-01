d7f184c6bcd3f15dadc81d4e0dba7f5a0e5a380c
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-06-01 12:46:04 +0300

    update

---
+++ django/course-note/012_Django_Auth_1/user_example/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import home_view, register,special
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('',home_view,name="home"),
+    path('special', special, name="special"),
+    path('register', register, name="register"),
+    path('change-password', auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html'), name="change-password"),
+]

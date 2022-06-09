c749e903bebd05007cb6fdf28de97ad46dde270e
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-06-09 23:45:12 +0300

    update v1

--- django/course-note/projects/weatherApp/weatherapp/views.py
+++ django/course-note/projects/weatherApp/weatherapp/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from decouple import config
+import requests
+from pprint import pprint
 
-def home(request):    
-    return render(request, "weatherapp/home.html")
+def home(request):
+    API_KEY=config('API_KEY')
+    city='yozgat'
+    url= f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+    reponse=requests.get(url)
+    content=reponse.json()
+
+    context={
+        'temp':content['main']['temp'],
+        'city':content['name'],
+        'desc':content['weather'][0]['description'],
+        'icon':content['weather'][0]['icon'],
+
+    }
+    
+    return render(request, "weatherapp/home.html",context)
+
+

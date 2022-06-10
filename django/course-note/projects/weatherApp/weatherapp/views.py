21b8e36e702243550527e5aa705619a6615f6621
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-06-10 23:11:22 +0300

    update v1

--- django/course-note/projects/weatherApp/weatherapp/views.py
+++ django/course-note/projects/weatherApp/weatherapp/views.py
@@ -2,22 +2,55 @@ from django.shortcuts import render
 from decouple import config
 import requests
 from pprint import pprint
+from django.contrib import messages
+from weatherapp.models import City
 
 def home(request):
+
     API_KEY=config('API_KEY')
-    city='yozgat'
-    url= f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
 
-    reponse=requests.get(url)
-    content=reponse.json()
+    u_city =request.GET.get('name')
+    # print(city)
 
-    context={
+    if u_city:
+        url= f"https://api.openweathermap.org/data/2.5/weather?q={u_city}&appid={API_KEY}&units=metric"
+        reponse=requests.get(url)
+        content=reponse.json()
+        if reponse.ok:
+            content=reponse.json()
+            r_city=content['name']
+            if City.objects.filter(name=r_city):
+                messages.warning(request,'City already exists!!')
+            else:
+                City.objects.create(name=r_city)
+                messages.success(request,'City created')
+        else:
+            messages.error(request,'There is no city')
+    
+    city_data=[]
+    cities=City.objects.all()
+
+    for city in cities:
+
+        url= f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+        reponse=requests.get(url)
+        content=reponse.json()
+        data={
         'temp':content['main']['temp'],
         'city':content['name'],
         'desc':content['weather'][0]['description'],
         'icon':content['weather'][0]['icon'],
 
+        }
+
+        city_data.append(data)
+        pprint(city_data)
+
+    context={
+        'city_data':city_data,
     }
+
+    
     
     return render(request, "weatherapp/home.html",context)
 

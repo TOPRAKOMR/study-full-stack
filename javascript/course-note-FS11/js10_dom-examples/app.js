209f5b10ee46abacadfa3027408259cbcf639ba2
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-08-25 17:21:58 +0300

    update

--- javascript/course-note-FS11/js10_dom-examples/app.js
+++ javascript/course-note-FS11/js10_dom-examples/app.js
@@ -1,12 +1,12 @@
-console.log('**** app.js *******');
+console.log("**** app.js *******");
 
-const dilInput = document.querySelector('.diller');
-const silBtn = document.getElementById('sil');
-const ekleBtn = document.getElementById('ekle');
+const dilInput = document.querySelector(".diller");
+const silBtn = document.getElementById("sil");
+const ekleBtn = document.getElementById("ekle");
 
-const dilSection = document.querySelector('#dil-section');
+const dilSection = document.querySelector("#dil-section");
 
-const ul = document.createElement('ul');
+const ul = document.createElement("ul");
 dilSection.appendChild(ul);
 
 //? SELECTORS
@@ -15,32 +15,31 @@ console.log(dilSection.children[0]);
 console.log(dilSection.parentNode.parentNode);
 
 // const h1 = ul.closest('.container').firstChild;
-const h1 = ul.closest('.container').firstElementChild;
+const h1 = ul.closest(".container").firstElementChild;
 console.log(h1);
-h1.style.color = 'red';
+h1.style.color = "red";
 
 //?container class ı içerisindeki btn class'ına ait olan elementleri arar.
-const buttons = ul.closest('.container').querySelectorAll('.btn');
+const buttons = ul.closest(".container").querySelectorAll(".btn");
 console.log(buttons);
 //************************************************* */
 
 ekleBtn.onclick = function () {
   if (!dilInput.value) {
-    alert('Lütfen bir dil giriniz');
+    alert("lütfen bir dil giriniz");
   } else {
-    ul.innerHTML += ` <li>${dilInput.value}</li>`;
-    dilInput.value = '';
-    javascriptKontrol();
+    ul.innerHTML += `<li>${dilInput.value}</li>`;
+    dilInput.value = "";
+    javakontrol();
   }
 };
 
-const javascriptKontrol = () => {
-  document.querySelectorAll('ul li').forEach((dil) => {
-    const kucukHarf = dil.textContent.toLowerCase();
-    if (kucukHarf === 'javascript') {
-      // dil.className = 'red';
-      //?Alternatif yöntem
-      dil.setAttribute('class', 'red');
+const javakontrol = () => {
+  document.querySelectorAll("ul li").forEach((dil) => {
+    const kucukharf = dil.textContent.toLowerCase();
+    if (kucukharf === "javascript") {
+      // dil.className='red';
+      dil.setAttribute("class", "red");
     }
   });
 };
@@ -48,21 +47,17 @@ const javascriptKontrol = () => {
 silBtn.onclick = function () {
   ul.childElementCount > 0
     ? ul.removeChild(ul.lastElementChild)
-    : alert('Silinecek dil kalmadi');
+    : alert("eleman kalmadı");
 };
 
-dilInput.addEventListener('keydown', (e) => {
-  // console.log(e);
-  if (e.keyCode === 13) {
+dilInput.addEventListener("keydown",(e)=>{
+  if(e.code==="Enter"){
     ekleBtn.onclick();
   }
-  // if (e.code === 'Enter') {
-  //   ekleBtn.onclick();
-  // }
-  if (e.code === 'Delete') {
+  if(e.code==="Delete"){
     silBtn.onclick();
   }
-});
+})
 
 window.onload = () => {
   javascriptKontrol();

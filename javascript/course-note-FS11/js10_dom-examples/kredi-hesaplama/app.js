b8b18ee1a6202950c52460393aad6ee3cf5548cb
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-08-26 20:45:36 +0300

    update

--- javascript/course-note-FS11/js10_dom-examples/kredi-hesaplama/app.js
+++ javascript/course-note-FS11/js10_dom-examples/kredi-hesaplama/app.js
@@ -5,6 +5,7 @@ const vade =document.querySelector("#vade");
 const tutar =document.querySelector("#tutar");
 const hesaplaBtn =document.querySelector(".btn");
 let oran=0;
+let taksit=0;
 
 
 hesaplaBtn.addEventListener("click",(e)=>{
@@ -18,5 +19,39 @@ hesaplaBtn.addEventListener("click",(e)=>{
   if(select.value==="arac kredisi"){
     oran=1.79;
   }
-  console.log(oran)
+  if (!select.value || !vade.value || !tutar.value) {
+    alert("lütfen kredi türü vade ve tutar giriniz")
+    
+  }
+  const faiz= oran/100;
+  taksit=tutar.value*(faiz*(1+faiz)**vade.value)/((1+faiz)**vade.value-1);
+  // console.log(taksit)
+  sonuclariGoster();
 })
+
+const sonuclariGoster=()=>{
+  const sonuclar=document.querySelector(".sonuclar");
+
+  sonuclar.innerHTML=`<table class="table table-bordered mt-4">
+  <tbody>
+  <tr>
+    <th>Kredi Miktarı</th>
+    <td>${tutar.value}</td>
+    <th>Kredi Tipi</th>
+    <td>${select.value}</td>
+  </tr>
+  <tr>
+    <th>Vade</th>
+    <td>${vade.value}</td>
+    <th>Faiz Oranı</th>
+    <td>${oran}</td>
+  </tr>
+  <tr>
+    <th>Toplam Tutar</th>
+    <td>${taksit*vade.value}</td>
+    <th>Taksit Tutarı</th>
+    <td>${taksit}</td>
+  </tr>
+</tbody></table>`
+
+};

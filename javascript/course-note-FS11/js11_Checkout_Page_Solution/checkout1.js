739305403024ec22bb2446b3be4ce7150eff9571
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-08-29 17:46:45 +0300

    update

---
+++ javascript/course-note-FS11/js11_Checkout_Page_Solution/checkout1.js
@@ -0,0 +1,10 @@
+const taxRate= 0.18;
+const shippingPrice=15.0;
+
+window.addEventListener("load",()=>{
+    localStorage.setItem("taxRate", taxRate);
+    localStorage.setItem("shippingPrice", shippingPrice);
+    sessionStorage.setItem("taxRate", taxRate);
+    sessionStorage.setItem("shippingPrice", shippingPrice);
+})
+

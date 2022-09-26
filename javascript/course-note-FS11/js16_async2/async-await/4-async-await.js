ae287456684d492f1b0ac46a4fd57b53db4c70a6
TOPRAKOMR <bilisimtoprak@gmail.com>
2022-09-26 17:02:20 +0300

    update

--- javascript/course-note-FS11/js16_async2/async-await/4-async-await.js
+++ javascript/course-note-FS11/js16_async2/async-await/4-async-await.js
@@ -17,42 +17,39 @@
 //* Await, promise-temelli herhangi bir fonksiyonun onune getirilerek getirildigi
 //* satirdaki kodun durudurulmasini saglar. Yapilan istek yerine getirilip sonuc
 //* degerlerinin dondurulmesine ile kodun calismasi devam eder.
-
-let hata = false;
-const getUsers = async function () {
+let hata =false;
+const getUsers = async function(){
   try {
-    const res = await fetch('https://api.github.com/users');
-    if (!res.ok) {
-      hata = true;
-      // throw new Error(`Something went wrong:${res.status}`);
-    }
-    const data = await res.json();
-    updateDom(data);
+  const res = await fetch('https://api.github.com/user');
+  if(!res.ok){
+    hata=true;
+    // throw new Error(`Something went wrong :${res.status}`);
+  }
+  const data =await res.json();
+  updateDom(data);
   } catch (error) {
-    console.log(error);
-  } finally {
-    hata = false;
+    console.log(error)
+    
   }
 };
 
 getUsers();
 
+
 const updateDom = (data) => {
   const userDiv = document.querySelector('.users');
-
-  if (hata) {
-    userDiv.innerHTML = `<h1 class="text-danger">Data can not be fetched</h1>
-    <img src="./img/404.png" alt="" />
-    `;
-  } else {
+  if(hata){
+    userDiv.innerHTML=`<h1 class="text-danger">Data can not be fetched</h1>`;
+  }
+  else{
     data.forEach((user) => {
-      //!destr
-      const { login, avatar_url, html_url } = user;
-      userDiv.innerHTML += `
+    //!destr
+    const { login, avatar_url, html_url } = user;
+    userDiv.innerHTML += `
     <h2 class="text-warning">NAME:${login}</h2>
     <img src=${avatar_url} width="50%" alt="" />
-    <h3>HTML_URL:${html_url}</h3>
-  `;
+    <h3>HTML_URL:${html_url}</h3>`;
     });
   }
-};
+  
+}
---
+++ javascript/course-note-FS11/js16_async2/flag-app/app1.js
@@ -0,0 +1,56 @@
+const fetchCountry = async (name) => {
+    const url =`https://restcountries.com/v3.1/name/${name}`;
+    try {
+        const res =await fetch(url);
+        if(!res.ok){
+            renderError(`Something went wrong: ${res.status}`);
+            throw new Error();
+        }
+        const data = await res.json();
+        // console.log(data[0])
+        renderCountry(data[0]);
+        
+    } catch (error) {
+        console.log(error)
+        
+    }
+
+
+
+};
+
+const renderCountry=(country)=>{
+console.log(country);
+const countriesDiv=document.querySelector('.countries');
+const {capital,name:{common},region,flags:{svg},languages,currencies}=country;
+// console.log(currencies)
+// console.log(Object.values(currencies)[0].name)
+countriesDiv.innerHTML += `
+<div class="card" style="width: 18rem;">
+  <img src="${svg}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${common}</h5>
+    <p class="card-text">${region}</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item"> <i class="fas fa-lg fa-landmark"></i> ${capital}</li>
+    <li class="list-group-item"> <i class="fas fa-lg fa-comments"></i> ${Object.values(languages)}</li>
+    <li class="list-group-item"> <i class="fas fa-lg fa-landmark"></i> ${Object.values(currencies)[0].name}, ${Object.values(currencies)[0].symbol} </li>
+  </ul>
+ 
+</div>
+
+`;
+
+}
+
+const renderError=(err)=>{
+const countriesDiv=document.querySelector('.countries')
+countriesDiv.innerHTML=`
+<h1 class="text-danger">${err}</h1>
+<img src="./img/404.png" alt="" />
+`;
+}
+
+fetchCountry("turkey");
+fetchCountry("usa");
